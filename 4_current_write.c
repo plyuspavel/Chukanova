3976bc1395b5a5f79d7f3047f2349e60303da3b8
plyuspavel <plyus.pavel2010@yandex.ru>
2016-09-22 19:06:24 +0400

    Add files via upload

--- 4_current_write.c
+++ 4_current_write.c
@@ -7,7 +7,8 @@
 #include <sys/stat.h>
 
 
-int main() {
+int main()
+{
     pid_t chpid;
     char name[] = "aaa.fifo";
     char str[] = "Hello!";
@@ -15,18 +16,51 @@ int main() {
     size_t size;
     int fd;
 
-    if (mknod(name, S_IFIFO | 0666, 0) < 0 && errno != EEXIST) {
+    if (mknod(name, S_IFIFO|0666, 0) < 0 && errno != EEXIST)
+    {
         printf("Can't create fifo\n");
         exit(-1);
     }
 
-
-    if ((fd = open(name, O_RDONLY)) < 0)
+    //chpid = fork();
+    /*
+    if (chpid < 0)
     {
-        printf("Can't open file fifo\n");
+        printf("Error fork");
         exit(-1);
     }
+     */
 
-    size = read(fd, restr, 7);
-    printf("Child has read restr: <%s>\n", restr);
+    //if (chpid > 0)
+    {
+        if ((fd = open(name, O_WRONLY)) < 0)
+        {
+            printf("Can't write\n");
+            exit(-1);
+
+        }
+
+        size = write(fd, str, 7);
+
+        if (size != 7)
+            printf("Can't write 7");
+
+        printf("Parent has wrote str in file\n");
+
+    }
+    //else
+    /*{
+        if ((fd = open(name, O_RDONLY)) < 0)
+        {
+            printf("Can't open file fifo\n");
+            exit(-1);
+        }
+
+        size = read(fd, restr, 7);
+        printf("Child has read restr: <%s>\n", restr);
+
+    }*/
+
+
+    return 0;
 }
