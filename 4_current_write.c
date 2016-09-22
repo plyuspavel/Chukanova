1eb826bbb82a45b062f3d626c5c8009d8d66e771
plyuspavel <plyus.pavel2010@yandex.ru>
2016-09-22 19:16:17 +0400

    Add files via upload

--- 4_current_write.c
+++ 4_current_write.c
@@ -16,51 +16,27 @@ int main()
     size_t size;
     int fd;
 
-    if (mknod(name, S_IFIFO|0666, 0) < 0 && errno != EEXIST)
+    if (mknod(name, S_IFIFO|0666, 0) < 0 && errno != EEXIST) //Инициализация fifo
     {
         printf("Can't create fifo\n");
         exit(-1);
     }
 
-    //chpid = fork();
-    /*
-    if (chpid < 0)
+
+
+    if ((fd = open(name, O_WRONLY)) < 0) //Открытие fifo для записи
     {
-        printf("Error fork");
+        printf("Can't write\n");
         exit(-1);
-    }
-     */
-
-    //if (chpid > 0)
-    {
-        if ((fd = open(name, O_WRONLY)) < 0)
-        {
-            printf("Can't write\n");
-            exit(-1);
-
-        }
-
-        size = write(fd, str, 7);
-
-        if (size != 7)
-            printf("Can't write 7");
-
-        printf("Parent has wrote str in file\n");
 
     }
-    //else
-    /*{
-        if ((fd = open(name, O_RDONLY)) < 0)
-        {
-            printf("Can't open file fifo\n");
-            exit(-1);
-        }
 
-        size = read(fd, restr, 7);
-        printf("Child has read restr: <%s>\n", restr);
+    size = write(fd, str, 7);         //Запись в fifo
 
-    }*/
+    if (size != 7)
+        printf("Can't write 7");
 
+    printf("Parent has wrote str in file\n");
 
     return 0;
 }
